d330b304e45f6268e2e05153961b329a4d343e02
Ben Fortuna <bfortuna@odecee.com.au>
2022-04-08 12:13:13 +1000

    Updated docs

--- ical4j-integration-api/src/main/java/org/ical4j/integration/CalendarListenerSupport.java
+++ ical4j-integration-api/src/main/java/org/ical4j/integration/CalendarListenerSupport.java
@@ -4,70 +4,153 @@ import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Property;
 import org.ical4j.integration.event.*;
 
+/**
+ * Provide support for notifying calendar listeners of events.
+ */
 public interface CalendarListenerSupport {
 
     ListenerList<Object> getCalendarListeners();
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.PUBLISH} events.
+     * @param listener a calendar publish listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarPublishListener(CalendarPublishListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.PUBLISH} events.
+     * @param listener a calendar publish listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarPublishListener(CalendarPublishListener listener) {
         return getCalendarListeners().remove(listener);
     }
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.REQUEST} events.
+     * @param listener a calendar request listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarRequestListener(CalendarRequestListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.REQUEST} events.
+     * @param listener a calendar request listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarRequestListener(CalendarRequestListener listener) {
         return getCalendarListeners().remove(listener);
     }
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.REPLY} events.
+     * @param listener a calendar reply listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarReplyListener(CalendarReplyListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.REPLY} events.
+     * @param listener a calendar reply listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarReplyListener(CalendarReplyListener listener) {
         return getCalendarListeners().remove(listener);
     }
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.ADD} events.
+     * @param listener a calendar add listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarAddListener(CalendarAddListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.ADD} events.
+     * @param listener a calendar add listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarAddListener(CalendarAddListener listener) {
         return getCalendarListeners().remove(listener);
     }
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.CANCEL} events.
+     * @param listener a calendar cancel listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarCancelListener(CalendarCancelListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.CANCEL} events.
+     * @param listener a calendar cancel listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarCancelListener(CalendarCancelListener listener) {
         return getCalendarListeners().remove(listener);
     }
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.REFRESH} events.
+     * @param listener a calendar refresh listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarRefreshListener(CalendarRefreshListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.REFRESH} events.
+     * @param listener a calendar refresh listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarRefreshListener(CalendarRefreshListener listener) {
         return getCalendarListeners().remove(listener);
     }
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.COUNTER} events.
+     * @param listener a calendar counter listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarCounterListener(CalendarCounterListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.COUNTER} events.
+     * @param listener a calendar counter listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarCounterListener(CalendarCounterListener listener) {
         return getCalendarListeners().remove(listener);
     }
 
+    /**
+     * Register listener for calendar {@link net.fortuna.ical4j.model.property.Method.DECLINE_COUNTER} events.
+     * @param listener a calendar decline counter listener
+     * @return true if listener was registered successfully
+     */
     default boolean addCalendarDeclineCounterListener(CalendarDeclineCounterListener listener) {
         return getCalendarListeners().add(listener);
     }
 
+    /**
+     * Unregister listener for calendar {@link net.fortuna.ical4j.model.property.Method.DECLINE_COUNTER} events.
+     * @param listener a calendar decline counter listener
+     * @return true if listener was unregistered successfully
+     */
     default boolean removeCalendarDeclineCounterListener(CalendarDeclineCounterListener listener) {
         return getCalendarListeners().remove(listener);
     }
