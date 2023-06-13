54ed714d278ec832c7a604aed47dd326323bfae8
Ben Fortuna <fortuna@micronode.com>
2023-06-13 19:01:01 +1000

    Support poll with timeout

--- ical4j-integration-api/src/main/java/org/ical4j/integration/local/LocalQueueAdapter.java
+++ ical4j-integration-api/src/main/java/org/ical4j/integration/local/LocalQueueAdapter.java
@@ -1,8 +1,12 @@
 package org.ical4j.integration.local;
 
 import org.ical4j.integration.ChannelAdapter;
+import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -22,14 +26,20 @@ public class LocalQueueAdapter<T> implements ChannelAdapter<T> {
     @Override
     public boolean receive(Consumer<T> consumer, long timeout, boolean autoExpunge) {
         T message;
-        if (autoExpunge) {
-            message = queue.poll();
-        } else {
-            message = queue.peek();
-        }
-        if (message != null) {
+        try {
+            if (autoExpunge) {
+                if (queue instanceof BlockingQueue) {
+                    message = Objects.requireNonNull(((BlockingQueue<T>) queue).poll(timeout, TimeUnit.SECONDS));
+                } else {
+                    message = Objects.requireNonNull(queue.poll());
+                }
+            } else {
+                message = Objects.requireNonNull(queue.peek());
+            }
             consumer.accept(message);
             return true;
+        } catch (InterruptedException | NullPointerException e) {
+            LoggerFactory.getLogger(LocalQueueAdapter.class).info("No data", e);
         }
         return false;
     }
---
+++ ical4j-integration-api/src/test/groovy/org/ical4j/integration/local/LocalQueueAdapterTest.groovy
@@ -0,0 +1,47 @@
+package org.ical4j.integration.local
+
+import net.fortuna.ical4j.model.Calendar
+import org.ical4j.integration.ChannelAdapter
+import spock.lang.Specification
+
+import java.util.concurrent.ArrayBlockingQueue
+
+class LocalQueueAdapterTest extends Specification {
+
+    def 'test send and receive calendar on local queue'() {
+        given: 'a local queue'
+        ChannelAdapter<Calendar> queue = new LocalQueueAdapter<>(new LinkedList<Calendar>())
+
+        when: 'a calendar is sent to the queue'
+        queue.send {new Calendar()}
+        Calendar received
+        boolean success = queue.receive({ c -> received = c }, 0)
+
+        then: 'it was received successfully'
+        success && received != null
+    }
+
+    def 'test receive calendar on empty local queue'() {
+        given: 'a local queue'
+        ChannelAdapter<Calendar> queue = new LocalQueueAdapter<>(new LinkedList<Calendar>())
+
+        when: 'a receive is attempted from the queue'
+        Calendar received
+        boolean success = queue.receive({ c -> received = c }, 0)
+
+        then: 'it was received successfully'
+        !success && received == null
+    }
+
+    def 'test receive calendar on blocking local queue'() {
+        given: 'a local queue'
+        ChannelAdapter<Calendar> queue = new LocalQueueAdapter<>(new ArrayBlockingQueue<Calendar>(1))
+
+        when: 'a receive is attempted from the queue with a 5 second timeout'
+        Calendar received
+        boolean success = queue.receive({ c -> received = c }, 5, true)
+
+        then: 'it was received successfully'
+        !success && received == null
+    }
+}
